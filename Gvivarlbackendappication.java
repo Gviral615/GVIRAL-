e356fd805a06ca09975bbf1891bf6ff38783285d
Gviral615 <louicinf@gmail.com>
2025-04-21 21:46:41 -0400

    Create Gvivarlbackendappication.java

---
+++ Gvivarlbackendappication.java
@@ -0,0 +1,11 @@
+src/main/java/com/gviral/package com.gviral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GviralBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GviralBackendApplication.class, args);
+    }
+}
