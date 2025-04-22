82ecd3b6527401612265a874a72f07a0f86a063e
Gviral615 <louicinf@gmail.com>
2025-04-21 22:09:15 -0400

    Update Gvivarlbackendappication.java

--- Gvivarlbackendappication.java
+++ Gvivarlbackendappication.java
@@ -1,11 +1,19 @@
 package com.gviral;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
-@SpringBootApplication
-public class GviralBackendApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(GviralBackendApplication.class, args);
+@RestController
+@RequestMapping("/api/music")
+public class MusicController {
+    @GetMapping
+    public List<String> getAllMusic() {
+        return Arrays.asList("Track 1", "Track 2");
+    }
+
+    @PostMapping("/upload")
+    public String uploadMusic(@RequestParam String title) {
+        // Simulate storing metadata
+        return "Music '" + title + "' uploaded successfully";
     }
 }
